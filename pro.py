7df5d7275efe118a2311884c29a47f3ab62d0347
maxijer1234@yandex.ru <maxijer1234>
2020-02-26 13:48:10 +0300

    add event and button

--- pro.py
+++ pro.py
@@ -15,6 +15,42 @@ class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi('new_company.ui', self)
+        self.evention.clicked.connect(self.super_event)
+
+    def add_event(self):
+        super().__init__()
+        self.z = Event()
+        self.z.show()
+
+    def super_event(self):
+        self.listWidget.clear()
+        z = Add_Event()
+        self.dobav_list(z)
+
+    def dobav_list(self, clas):  # добавляем виджеты на главный экран
+        item = QListWidgetItem(self.listWidget)
+        self.mywid = clas
+        self.listWidget.addItem(item)
+        self.listWidget.setItemWidget(item, self.mywid)
+        item.setSizeHint(self.mywid.size())
+
+
+class Add_Event(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('add_event.ui', self)
+        self.pushButton.clicked.connect(self.prikol)
+
+    def prikol(self):
+        super().__init__()
+        self.z = Event()
+        self.z.show()
+
+
+class Event(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('zametka.ui', self)
 
 
 app = QApplication(sys.argv)
